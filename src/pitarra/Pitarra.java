2aa1634d12cc7c9bfad18b5c263d841ce1b2ca8c
maekhamin <maekhamin@gmail.com>
2012-07-16 21:15:36 +0000

    added menu for the frame

--- src/pitarra/Pitarra.java
+++ src/pitarra/Pitarra.java
@@ -3,17 +3,22 @@ package pitarra;
 import javax.swing.JFrame;
 
 public class Pitarra {
-	public static void main(String[] args) {
-		JFrame frame = new JFrame(PitCons.title);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(PitCons.initialWindowSize);
+        public static void main(String[] args) {
+        		Menu menu;
+        		GamePanel gPanel;
+        		gPanel = new GamePanel(PitCons.gameBackdrop);
+        		menu = new Menu(gPanel);
+                JFrame frame = new JFrame(PitCons.title);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setPreferredSize(PitCons.initialWindowSize);
 
-		frame.getContentPane().add(new GamePanel(PitCons.gameBackdrop));
-		frame.pack();
-		frame.setLocation(PitCons.initialWindowPosition);
+                frame.getContentPane().add(gPanel);
+                frame.pack();
+                frame.setLocation(PitCons.initialWindowPosition);
+                frame.setJMenuBar(menu.getMenuBar());
 
-		frame.setResizable(true);
-		frame.setVisible(true);
-	}
+                frame.setResizable(true);
+                frame.setVisible(true);
+        }
 
 }
