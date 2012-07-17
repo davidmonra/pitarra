fa45e94e88d460dbe66211c1fd943e9a47b45c1d
jswank2067@gmail.com <jswank2067@gmail.com@0dc5c7eb-4402-b63d-2e69-cbe20ea613c1>
2012-07-17 19:52:01 +0000

    Made changes to Vertex, PyramidPanel, and GamePanel to play Traditional game. Still need to implement Moving the pieces.

--- trunk/src/pitarra/GamePanel.java
+++ trunk/src/pitarra/GamePanel.java
@@ -10,74 +10,145 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class GamePanel extends JPanel {
-        private ImageIcon backdrop;
-        private PyramidPanel pyramid;
-        //private ButtonPanel buttons;
-        //private Menu menu;
-        private boolean isPlayer1Turn;
-        private boolean playBasicGame = true;
+	private ImageIcon backdrop;
+	private PyramidPanel pyramid;
+	private boolean isPlayer1Turn;
+	private boolean basicGame;
+	private boolean takeNextPiece; // if true, next piece clicked is removed
+	private int p1PiecesLeft, p2PiecesLeft, p1PiecesLost, p2PiecesLost;
 
-        public GamePanel(ImageIcon backdrop) {
-                super();
-                this.backdrop = backdrop;
-                this.isPlayer1Turn = true;
-                setLayout(new BorderLayout());
-                setPreferredSize(new Dimension(
-                                (int) PitCons.initialWindowSize.getWidth(),
-                                (int) PitCons.initialWindowSize.getHeight()));
-                setDoubleBuffered(true);
+	public GamePanel(ImageIcon backdrop) {
+		super();
+		this.backdrop = backdrop;
+		this.pyramid = new PyramidPanel(PitCons.pyramidBackdrop,
+				PitCons.initialPyramidPosition.x,
+				PitCons.initialPyramidPosition.y, PitCons.initialPyramidSize,
+				PitCons.pyramidLineColor, this);
+		this.isPlayer1Turn = true;
+		this.basicGame = true;
+		this.takeNextPiece = false;
+		this.p1PiecesLeft = 12;
+		this.p2PiecesLeft = 12;
+		this.p1PiecesLost = 0;
+		this.p2PiecesLost = 0;
 
-                pyramid = new PyramidPanel(PitCons.pyramidBackdrop,
-                                PitCons.initialPyramidPosition.x,
-                                PitCons.initialPyramidPosition.y, PitCons.initialPyramidSize,
-                                PitCons.pyramidLineColor, this);
-                add(pyramid, BorderLayout.CENTER);
+		setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(
+				(int) PitCons.initialWindowSize.getWidth(),
+				(int) PitCons.initialWindowSize.getHeight()));
+		setDoubleBuffered(true);
 
-                //buttons = new ButtonPanel(this);
-                //add(buttons, BorderLayout.NORTH);
-                
-                //menu = new Menu(this);
-                //add(menu, BorderLayout.NORTH);
-        }
+		add(pyramid, BorderLayout.CENTER);
+	}
 
-        public boolean isPlayer1Turn() {
-                return isPlayer1Turn;
-        }
+	public void resetGame() {
+		this.p1PiecesLeft = 12;
+		this.p2PiecesLeft = 12;
+		this.p1PiecesLost = 0;
+		this.p2PiecesLost = 0;
+		this.pyramid.resetPyramid();
+		setPlayer1Turn(true);
+		setRemoveNextPiece(false);
+	}
 
-        public void switchPlayer() {
-                this.isPlayer1Turn = !isPlayer1Turn;
-        }
+	public boolean isPlayer1Turn() {
+		return isPlayer1Turn;
+	}
 
-        public PyramidPanel getPyramid() {
-                return pyramid;
-        }
+	public void setPlayer1Turn(boolean isPlayer1Turn) {
+		this.isPlayer1Turn = isPlayer1Turn;
+	}
 
-        public void showMessage(String message) {
-                JOptionPane.showMessageDialog(this, message);
-        }
-        
-        public void weGotWinner(int winner){
-        	String asdf = "player " + winner + " is the winner!";
-        	System.out.println("player " + winner + " is the winner!");
-        	showMessage(asdf);
-        }
+	public void switchPlayer() {
+		this.isPlayer1Turn = !isPlayer1Turn;
+	}
 
-        public void paintComponent(Graphics page) {
-                super.paintComponent(page);
-                // draw backdrop
-                if (backdrop != null) {
-                        page.drawImage(backdrop.getImage(), 0, 0, getWidth(), getHeight(),
-                                        0, 0, backdrop.getIconWidth(), backdrop.getIconHeight(),
-                                        this);
-                }
-        }
+	public PyramidPanel getPyramid() {
+		return pyramid;
+	}
 
-		public void setPlayBasicGame(boolean playBasicGame) {
-			this.playBasicGame = playBasicGame;
+	public boolean isBasicGame() {
+		return basicGame;
+	}
+
+	public int getPiecesLeft(int playerNumber) {
+		switch (playerNumber) {
+		case 1:
+			return p1PiecesLeft;
+		case 2:
+			return p2PiecesLeft;
+		default:
+			throw new IllegalArgumentException("Player number must be 1 or 2.");
 		}
+	}
 
-		public boolean isPlayBasicGame() {
-			return playBasicGame;
+	// decrements the number of pieces left by 1
+	public void decrPiecesLeft(int playerNumber) {
+		switch (playerNumber) {
+		case 1:
+			p1PiecesLeft--;
+			break;
+		case 2:
+			p2PiecesLeft--;
+			break;
+		default:
+			throw new IllegalArgumentException("Player number must be 1 or 2.");
 		}
+	}
+
+	public int getPiecesLost(int playerNumber) {
+		switch (playerNumber) {
+		case 1:
+			return p1PiecesLost;
+		case 2:
+			return p2PiecesLost;
+		default:
+			throw new IllegalArgumentException("Player number must be 1 or 2.");
+		}
+	}
+
+	// increments the number of pieces lost by 1
+	public void incrPiecesLost(int playerNumber) {
+		switch (playerNumber) {
+		case 1:
+			p1PiecesLost++;
+			break;
+		case 2:
+			p2PiecesLost++;
+			break;
+		default:
+			throw new IllegalArgumentException("Player number must be 1 or 2.");
+		}
+	}
+
+	public void setBasicGame(boolean basicGame) {
+		this.basicGame = basicGame;
+	}
+
+	public boolean isRemoveNextPiece() {
+		return takeNextPiece;
+	}
+
+	public void setRemoveNextPiece(boolean removeNextPiece) {
+		this.takeNextPiece = removeNextPiece;
+	}
+
+	public void showMessage(String message) {
+		JOptionPane.showMessageDialog(this, message);
+	}
+
+	public void weGotWinner(int winner) {
+		String asdf = "player " + winner + " is the winner!";
+		showMessage(asdf);
+	}
+
+	public void paintComponent(Graphics page) {
+		// draw backdrop
+		if (backdrop != null) {
+			page.drawImage(backdrop.getImage(), 0, 0, getWidth(), getHeight(),
+					0, 0, backdrop.getIconWidth(), backdrop.getIconHeight(),
+					this);
+		}
+	}
 
 }
