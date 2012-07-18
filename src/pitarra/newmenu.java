865bab7b24d3dff465e34ae898d70485c00c4403
bkafroboy69 <bkafroboy69@yahoo.com>
2012-07-18 21:58:51 +0000

    got the panel to work it just prints the instructions on one line need to figure out how to make it print on newlines

--- src/pitarra/newmenu.java
+++ src/pitarra/newmenu.java
@@ -1,36 +1,32 @@
+import javax.swing.*;
 import java.io.*;
-
-import javax.swing.JFrame;
-public class newmenu {
-    
-    
- public static void main(String args[]){
-  try{
-  // Open file
-  FileInputStream fstream = new FileInputStream("example.txt");
-  // Grab the file with DataInputStream
-  DataInputStream input = new DataInputStream(fstream);
-  BufferedReader buff = new BufferedReader(new InputStreamReader(input));
-  String stringLine;
-  // Read the file line by line
-  while ((stringLine = buff.readLine()) != null)   {
-  // Print the content on the console
-  //System.out.println (stringLine);
-      JFrame frame = new JFrame("Pitarra");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      newmenu ta = new newmenu();
-      frame.pack();
-     
-      frame.setSize(450, 600);
-      frame.setVisible(true);
-
-
-  }
-  //Close the input stream
-  input.close();
-    }catch (Exception e){
-    //Catch exception if any
-  System.err.println("Error: " + e.getMessage());
-  }
-  }
-}
+import java.lang.*;
+import java.awt.*;
+class MegaViewer1 extends JFrame
+{
+    JTabbedPane jtp1=new JTabbedPane();
+    JPanel jp1=new JPanel();
+    JTextArea t1=new JTextArea();
+    MegaViewer1() throws Exception
+    {
+        super("Pitarra");
+        FileInputStream  f=new FileInputStream ("example.txt");
+        DataInputStream input = new DataInputStream(f);
+        BufferedReader brk=new BufferedReader(new InputStreamReader(input));       
+        String s;
+        while((s=brk.readLine())!=null){
+        t1.append(s);
+        }
+        jp1.add(t1);
+        jtp1.addTab("How to play",t1);
+        add(jtp1);  
+    }
+    public static void main(String args[]) throws Exception
+    {
+        MegaViewer1 mv1=new MegaViewer1();
+       //mv1.pack();
+        mv1.setSize(450, 600);
+        mv1.setVisible(true);   
+        mv1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    }
+}    
