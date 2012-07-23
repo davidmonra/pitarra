c6dc38f4d742ff070c7b2022eda62e35a8b3868f
maekhamin <maekhamin@gmail.com>
2012-07-23 20:34:14 +0000

    added alot of stuff.

--- src/pitarra/Menu.java
+++ src/pitarra/Menu.java
@@ -10,121 +10,153 @@ import javax.swing.JMenuItem;
 import javax.swing.ButtonGroup;
 
 public class Menu extends JMenuBar {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -9120600991573125349L;
-	private JMenuBar menuBar;
-	private GamePanel gPanel;
-	private JMenu menuMain, menuGame;
-	private JMenuItem Exit, Instruction;
-	// action listener for traditional game option, beginner game option, exit,
-	// sound, and instructions
-	private ActionListener alTrad, alBeg, alEx, alSound, alInstruction;
-	private JCheckBoxMenuItem sound, NewTrad, NewBeg;
-	private ButtonGroup menuGroupGame;
+        /**
+         * 
+         */
+        private static final long serialVersionUID = -9120600991573125349L;
+        private JMenuBar menuBar;
+        private GamePanel gPanel;
+        private JMenu menuMain, menuGame, menuHelp;
+        private JMenuItem Exit, Instruction, ResetGame, About;
+        // action listener for traditional game option, beginner game option, exit,
+        // sound, and instructions
+        private ActionListener alTrad, alBeg, alEx, alSound, alInstruction, alAbout, alReset;
+        private JCheckBoxMenuItem sound, NewTrad, NewBeg;
+        private ButtonGroup menuGroupGame;
 
-	public Menu(GamePanel game) {
-		this.gPanel = game;
+        public Menu(GamePanel game) {
+                this.gPanel = game;
 
-		// menu bar
-		this.menuBar = new JMenuBar();
+                // menu bar
+                this.menuBar = new JMenuBar();
 
-		// menu bar options
-		this.menuMain = new JMenu("Menu");
-		this.menuGame = new JMenu("Game");
+                // menu bar options
+                this.menuMain = new JMenu("File");
+                this.menuGame = new JMenu("Game");
+                this.menuHelp = new JMenu("Help");
 
-		// menu items
-		this.NewBeg = new JCheckBoxMenuItem("New Beginner Game");
-		this.NewTrad = new JCheckBoxMenuItem("New Traditional Game");
-		this.sound = new JCheckBoxMenuItem("Sound");
-		this.Exit = new JMenuItem("Exit");
-		this.Instruction = new JMenuItem("Instruction");
+                // menu items
+                this.NewBeg = new JCheckBoxMenuItem(PitCons.menuNewBeg);
+                this.NewTrad = new JCheckBoxMenuItem(PitCons.menuNewTrad);
+                this.sound = new JCheckBoxMenuItem(PitCons.menuSound);
+                this.Exit = new JMenuItem(PitCons.menuExit);
+                this.Instruction = new JMenuItem(PitCons.menuInstruction);
+                this.ResetGame = new JMenuItem(PitCons.menuResetGame);
+                this.About = new JMenuItem(PitCons.menuAbout);
 
-		// menu grouping to make game mode mutually exclusive
-		this.menuGroupGame = new ButtonGroup();
+                // menu grouping to make game mode mutually exclusive
+                this.menuGroupGame = new ButtonGroup();
 
-		this.alTrad = new ButtonActionNewGameT();
-		this.alBeg = new ButtonActionNewGameB();
-		this.alEx = new ButtonActionExit();
-		this.alSound = new ButtonActionSound();
-		this.alInstruction = new ButtonActionInstruction();
+                this.alTrad = new ButtonActionNewGameT();
+                this.alBeg = new ButtonActionNewGameB();
+                this.alEx = new ButtonActionExit();
+                this.alSound = new ButtonActionSound();
+                this.alInstruction = new ButtonActionInstruction();
+                this.alAbout = new ButtonActionAbout();
+                this.alReset = new ButtonActionResetGame();
 
-		this.NewTrad.addActionListener(alTrad);
-		this.NewBeg.addActionListener(alBeg);
-		this.Exit.addActionListener(alEx);
-		this.sound.addActionListener(alSound);
-		this.Instruction.addActionListener(alInstruction);
-		setMenu();
-	}
+                this.NewTrad.addActionListener(alTrad);
+                this.NewBeg.addActionListener(alBeg);
+                this.Exit.addActionListener(alEx);
+                this.sound.addActionListener(alSound);
+                this.Instruction.addActionListener(alInstruction);
+                this.ResetGame.addActionListener(alReset);
+                this.About.addActionListener(alAbout);
+                
+                setMenu();
+        }
 
-	public JMenuBar getMenuBar() {
-		return menuBar;
-	}
+        public JMenuBar getMenuBar() {
+                return menuBar;
+        }
 
-	private void setMenu() {
-		menuBar.add(menuMain);
-		menuBar.add(menuGame);
+        private void setMenu() {
+                menuBar.add(menuMain);
+                menuBar.add(menuGame);
+                menuBar.add(menuHelp);
 
-		menuGroupGame.add(NewTrad);
-		menuGroupGame.add(NewBeg);
-		menuGroupGame.setSelected(NewBeg.getModel(), true);
+                menuGroupGame.add(NewTrad);
+                menuGroupGame.add(NewBeg);
+                menuGroupGame.setSelected(NewBeg.getModel(), true);
 
-		menuGame.add(NewBeg);
-		menuGame.add(NewTrad);
-		menuGame.addSeparator();
-		menuGame.add(sound);
-		menuGame.addSeparator();
-		menuGame.add(Instruction);
-		sound.setSelected(true);
+                menuGame.add(NewBeg);
+                menuGame.add(NewTrad);
+                menuGame.addSeparator();
+                menuGame.add(sound);
+                menuGame.addSeparator();
+                menuGame.add(Instruction);
+                sound.setSelected(true);
+                
+                menuHelp.add(About);
 
-		menuMain.add(Exit);
-	}
+                menuMain.add(ResetGame);
+                menuMain.addSeparator();
+                menuMain.add(Exit);
+        }
 
-	private class ButtonActionNewGameT implements ActionListener {
+        private class ButtonActionAbout implements ActionListener{
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			gPanel.setBasicGame(false);
-			gPanel.resetGame();
-		}
-	}
-
-	private class ButtonActionNewGameB implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			gPanel.setBasicGame(true);
-			gPanel.resetGame();
-		}
-	}
-
-	private class ButtonActionExit implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.exit(0);
-		}
-	}
-
-	private class ButtonActionSound implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			if (gPanel.isSoundOn()) {
-				gPanel.setSoundOn(false);
-			} else {
-				gPanel.setSoundOn(true);
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//PitCons.aboutText
+				gPanel.showMessage(PitCons.aboutText, "About Pitarra");
 			}
-		}
-	}
+        	
+        }
+        
+        private class ButtonActionResetGame implements ActionListener{
 
-	private class ButtonActionInstruction implements ActionListener {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				gPanel.resetGame();
+			}
+        	
+        }
+        
+        private class ButtonActionNewGameT implements ActionListener {
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			gPanel.showInstructions();
-		}
-	}
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                        gPanel.setBasicGame(false);
+                        gPanel.resetGame();
+                }
+        }
+
+        private class ButtonActionNewGameB implements ActionListener {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                        gPanel.setBasicGame(true);
+                        gPanel.resetGame();
+                }
+        }
+
+        private class ButtonActionExit implements ActionListener {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                        System.exit(0);
+                }
+        }
+
+        private class ButtonActionSound implements ActionListener {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                        if (gPanel.isSoundOn()) {
+                                gPanel.setSoundOn(false);
+                        } else {
+                                gPanel.setSoundOn(true);
+                        }
+                }
+        }
+
+        private class ButtonActionInstruction implements ActionListener {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                        gPanel.showInstructions();
+                }
+        }
 
 }
