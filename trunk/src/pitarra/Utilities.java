9f05c2095e95df3472b3f6d7466739d93b2558c0
maekhamin@gmail.com <maekhamin@gmail.com@0dc5c7eb-4402-b63d-2e69-cbe20ea613c1>
2012-07-28 01:56:50 +0000

    edited indentation

--- trunk/src/pitarra/Utilities.java
+++ trunk/src/pitarra/Utilities.java
@@ -10,29 +10,29 @@ import javax.swing.JPanel;
 
 public final class Utilities {
 
-	public static final String readFile(String pathAndFilename) {
-		File file = new File(pathAndFilename);
+        public static final String readFile(String pathAndFilename) {
+                File file = new File(pathAndFilename);
 
-		if (file.canRead()) {
-			Scanner scan;
-			String fileContents = "";
+                if (file.canRead()) {
+                        Scanner scan;
+                        String fileContents = "";
 
-			try {
-				scan = new Scanner(file);
-			} catch (FileNotFoundException e) {
-				return null;
-			}
+                        try {
+                                scan = new Scanner(file);
+                        } catch (FileNotFoundException e) {
+                                return null;
+                        }
 
-			while (scan.hasNextLine())
-				fileContents += scan.nextLine() + "\n";
+                        while (scan.hasNextLine())
+                                fileContents += scan.nextLine() + "\n";
 
-			return fileContents;
-		} else {
-			return null;
-		}
-	}
-   
-   public static final String[] readFilePerLine(String pathAndFilename) {
+                        return fileContents;
+                } else {
+                        return null;
+                }
+        }
+
+        public static final String[] readFilePerLine(String pathAndFilename) {
             File file = new File(pathAndFilename);
             final String separator = "---";
             String forCompare = "";
@@ -76,17 +76,17 @@ public final class Utilities {
                     return null;
             }
     }
+        
+        public static void drawBackdrop(Graphics page, ImageIcon backdrop,
+                        int width, int height, JPanel panel) {
+                if (backdrop != null) {
+                        page.drawImage(backdrop.getImage(), 0, 0, width, height, 0, 0,
+                                        backdrop.getIconWidth(), backdrop.getIconHeight(), panel);
+                }
+        }
 
-	public static void drawBackdrop(Graphics page, ImageIcon backdrop,
-			int width, int height, JPanel panel) {
-		if (backdrop != null) {
-			page.drawImage(backdrop.getImage(), 0, 0, width, height, 0, 0,
-					backdrop.getIconWidth(), backdrop.getIconHeight(), panel);
-		}
-	}
+        // static class is never instantiated
+        private Utilities() {
 
-	// static class is never instantiated
-	private Utilities() {
-
-	}
+        }
 }
