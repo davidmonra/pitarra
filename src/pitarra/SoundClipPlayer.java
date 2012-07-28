143bdc624c9b4eb2c0d64081118e7ca7e061654b
jleija <jleija@cs.utsa.edu>
2012-07-28 00:48:04 +0000

    method changes

--- src/pitarra/SoundClipPlayer.java
+++ src/pitarra/SoundClipPlayer.java
@@ -10,12 +10,8 @@ public class SoundClipPlayer extends JFrame {
    public Clip sClip;
 
 // Constructor
-   public SoundClipPlayer(String soundFileName, int howMany) {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      this.setTitle("Test Sound Clip");
-      this.setSize(300, 200);
-      this.setVisible(true);//can set to false if you want to make it invisible
-      
+   public SoundClipPlayer(String soundFileName) {
+      this.setVisible(false);
       try {
          // Open an audio input stream.
     	 File soundFile = new File(soundFileName);
@@ -24,12 +20,6 @@ public class SoundClipPlayer extends JFrame {
          sClip = AudioSystem.getClip();
          // Open audio clip and load samples from the audio input stream.
          sClip.open(audioIn);
-         if(howMany==1)
-        	 sClip.start(); //play one time
-         else
-        	 sClip.loop(Clip.LOOP_CONTINUOUSLY);  // repeat forever
-         //you can use loop to change how many times it plays if you want 
-         //I figured we only needed once and repeat
          
       } catch (UnsupportedAudioFileException e) {
     	  System.out.println("sound error 1");
@@ -43,10 +33,10 @@ public class SoundClipPlayer extends JFrame {
    public void stop(){//to stop it 
 	   sClip.stop();
    }
-   public void playItAgain(){//play it again
-	   sClip.start();
+   public void play(){//play it once
+	   sClip.loop(1);
    }
-   public void playItForever(){//loop it again
+   public void playItForever(){//loop it 
 	   sClip.loop(Clip.LOOP_CONTINUOUSLY);
    }
    
