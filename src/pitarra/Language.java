d72a80b0ec93c19e7f25522e0bfc5348ab3892ba
maekhamin <maekhamin@gmail.com>
2012-07-28 03:11:55 +0000

    for menu only

---
+++ src/pitarra/Language.java
@@ -0,0 +1,36 @@
+package pitarra;
+
+public final class Language {
+	protected static boolean langEng = true;
+	
+	protected final static String menuNewBegEng = "New Beginner Game";
+	protected final static String menuNewTradEng = "New Traditional Game";
+	protected final static String menuSoundEng = "Sound";
+	protected final static String menuExitEng = "Exit";
+	protected final static String menuInstructionEng = "Instruction";
+	protected final static String menuResetGameEng = "Reset Game";
+	protected final static String menuAboutEng = "About";
+	protected final static String menuLanguageEng = "Languages";
+	protected final static String menuFileEng = "File";
+	protected final static String menuGameEng = "Game";
+	protected final static String menuHelpEng = "Help";
+	protected final static String menuEnglishEng = "English";
+	protected final static String menuSpanishEng = "Español";
+	
+	protected final static String menuNewBegSpa = "Nuevo Juego de Principiante";
+	protected final static String menuNewTradSpa = "Juego Tradicional de Nueva";
+	protected final static String menuSoundSpa = "Sonido";
+	protected final static String menuExitSpa = "Salida";
+	protected final static String menuInstructionSpa = "Instrucción";
+	protected final static String menuResetGameSpa = "Restablecer Juego";
+	protected final static String menuAboutSpa = "Referencia";
+	protected final static String menuLanguageSpa = "Lengua";
+	protected final static String menuFileSpa = "Ficha";
+	protected final static String menuGameSpa = "Juego";
+	protected final static String menuHelpSpa = "Ayuda";
+	protected final static String menuEnglishSpa = "English";
+	protected final static String menuSpanishSpa = "Español";
+	
+	private Language(){
+	}
+}
