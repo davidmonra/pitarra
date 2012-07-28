9b0af56562f5965a48cf46272b35f0bd92974cbb
maekhamin <maekhamin@gmail.com>
2012-07-28 23:57:10 +0000

    added some stuff

--- src/pitarra/Language.java
+++ src/pitarra/Language.java
@@ -16,6 +16,11 @@ public final class Language {
 	protected final static String menuHelpEng = "Help";
 	protected final static String menuEnglishEng = "English";
 	protected final static String menuSpanishEng = "Español";
+	protected final static String menuMusicEng = "Music";
+	protected final static String playerStringEng = "Player";
+	protected final static String playerStringACEng = "PLAYER";
+	protected final static String pieceCountTextEng = " Left";
+	protected final static String pieceLostTextEng = " lost";
 	
 	protected final static String menuNewBegSpa = "Nuevo Juego de Principiante";
 	protected final static String menuNewTradSpa = "Nuevo Juego de Tradicional";
@@ -30,6 +35,24 @@ public final class Language {
 	protected final static String menuHelpSpa = "Ayuda";
 	protected final static String menuEnglishSpa = "English";
 	protected final static String menuSpanishSpa = "Español";
+	protected final static String menuMusicSpa = "Música";
+	protected final static String playerStringSpa = "Jugador";
+	protected final static String playerStringACSpa = "JUGADOR";
+	protected final static String pieceCountTextSpa = " izquierda";
+	protected final static String pieceLostTextSpa = " perdido";
+	
+	public static String playerString = playerStringEng;
+	public static String playerStringAC = playerStringACEng;
+	public static String pieceCountText = pieceCountTextEng;
+	public static String pieceLostText = pieceLostTextEng;
+	
+	public void setPlayerString(String st){
+		playerString = st;
+	}
+	
+	public void setPlayerStringAC(String st){
+		playerStringAC = st;
+	}
 	
 	private Language(){
 	}
