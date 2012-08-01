fb60314bc0520fa3b23960aba9588ba8e8000a39
maekhamin <maekhamin@gmail.com>
2012-08-01 20:24:11 +0000

    added stuff

--- src/pitarra/PlayerPanel.java
+++ src/pitarra/PlayerPanel.java
@@ -14,136 +14,165 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class PlayerPanel extends JPanel {
-	private GamePanel gPanel;
-	private JLabel titleLabel, nameLabel, piecesLeft, piecesLost;
-	private JPanel namePanel;
-	private String player;
-	private String playerName;
-	private ImageIcon backdrop;
-	private Color playerColor;
-	private boolean showColorText;
+        private GamePanel gPanel;
+        private JLabel titleLabel, nameLabel, piecesLeft, piecesLost, numWins;
+        private JPanel namePanel;
+        private String player;
+        private String playerName;
+        private ImageIcon backdrop;
+        private Color playerColor;
+        private boolean showColorText;
+		private boolean nameLock = false;
+        private int pNum;
 
-	public PlayerPanel(ImageIcon backdrop, int playerNumber, Color playerColor,
-			GamePanel panel) {
-		this.gPanel = panel;
-		this.titleLabel = new JLabel("PLAYER " + playerNumber, JLabel.CENTER);
-		this.nameLabel = new JLabel(PitCons.nameFieldPrompt, JLabel.CENTER);
-		this.piecesLeft = new JLabel(gPanel.getPiecesLeft(playerNumber)
-				+ PitCons.pieceCountText, JLabel.CENTER);
-		this.piecesLost = new JLabel(gPanel.getPiecesLost(playerNumber)
-				+ PitCons.pieceLostText, JLabel.CENTER);
-		this.namePanel = new JPanel();
-		this.player = "Player " + playerNumber;
-		this.playerName = PitCons.nameFieldPrompt;
-		this.setOpaque(false);
-		this.backdrop = backdrop;
-		this.playerColor = playerColor;
-		this.showColorText = true;
+        public PlayerPanel(ImageIcon backdrop, int playerNumber, Color playerColor,
+                        GamePanel panel) {
+                this.gPanel = panel;
+                this.titleLabel = new JLabel(Language.playerStringAC + playerNumber, JLabel.CENTER);
+                this.nameLabel = new JLabel(Language.nameFieldPrompt, JLabel.CENTER);
+				this.numWins = new JLabel(Language.numWinsText+gPanel.getNumWins(playerNumber),JLabel.CENTER);
+                this.piecesLeft = new JLabel(gPanel.getPiecesLeft(playerNumber)
+                                + Language.pieceCountText, JLabel.CENTER);
+                this.piecesLost = new JLabel(gPanel.getPiecesLost(playerNumber)
+                                + Language.pieceLostText, JLabel.CENTER);
+                this.namePanel = new JPanel();
+                this.player = Language.playerString + playerNumber;
+                this.playerName = Language.nameFieldPrompt;
+                this.setOpaque(false);
+                this.backdrop = backdrop;
+                this.playerColor = playerColor;
+                this.showColorText = true;
+                this.pNum = playerNumber;
 
-		// titleLabel appearance
-		titleLabel.setOpaque(false);
-		titleLabel.setFont(PitCons.italicBoldFont);
+                // titleLabel appearance
+                titleLabel.setOpaque(false);
+                titleLabel.setFont(PitCons.italicBoldFont);
 
-		// nameLabel appearance
-		nameLabel.setOpaque(false);
-		nameLabel.setFont(PitCons.boldFont);
-		nameLabel.addMouseListener(new PlayerListener());
+                // nameLabel appearance
+                nameLabel.setOpaque(false);
+                nameLabel.setFont(PitCons.boldFont);
+                nameLabel.addMouseListener(new PlayerListener());
 
-		// pieceCount appearance
-		piecesLeft.setOpaque(false);
-		piecesLeft.setFont(PitCons.italicBoldFont);
+                // pieceCount appearance
+                piecesLeft.setOpaque(false);
+                piecesLeft.setFont(PitCons.italicBoldFont);
 
-		// pieceCount appearance
-		piecesLost.setOpaque(false);
-		piecesLost.setFont(PitCons.italicBoldFont);
+                // pieceCount appearance
+                piecesLost.setOpaque(false);
+                piecesLost.setFont(PitCons.italicBoldFont);
+				
+				// numWins apperarance
+				numWins.setOpaque(false);
+				numWins.setFont(PitCons.italicBoldFont);
 
-		// namePanel appearance
-		namePanel.setOpaque(false);
-		namePanel.setPreferredSize(new Dimension(
-				PitCons.initialPlayerPanelWidth,
-				PitCons.initialPlayerPanelWidth * 2 / 3));
+                // namePanel appearance
+                namePanel.setOpaque(false);
+                namePanel.setPreferredSize(new Dimension(
+                                PitCons.initialPlayerPanelWidth,
+                                PitCons.initialPlayerPanelWidth * 2 / 3));
 
-		setPiecesLeft(PitCons.initialNumberOfPieces);
-		namePanel.setLayout(new GridLayout(4, 1));
+                setPiecesLeft(PitCons.initialNumberOfPieces);
+                namePanel.setLayout(new GridLayout(5, 1));
 
-		// add labels to namePanel
-		namePanel.add(titleLabel);
-		namePanel.add(nameLabel);
-		namePanel.add(piecesLeft);
-		namePanel.add(piecesLost);
+                // add labels to namePanel
+                namePanel.add(titleLabel);
+                namePanel.add(nameLabel);
+                namePanel.add(piecesLeft);
+                namePanel.add(piecesLost);
+                namePanel.add(numWins);
 
-		add(namePanel);
-	}
+                add(namePanel);
+        }
+		
+		public void updateLabels(){
+        	titleLabel.setText(Language.playerStringAC + " " + pNum);
+        	piecesLeft.setText(gPanel.getPiecesLeft(pNum) + Language.pieceCountText);
+        	piecesLost.setText(gPanel.getPiecesLost(pNum) + Language.pieceLostText);
+        	if(!nameLock){
+        		nameLabel.setText(Language.nameFieldPrompt);
+        	}
+        	player = Language.playerString + " " + pNum;
+        	numWins.setText(Language.numWinsText+gPanel.getNumWins(pNum));
+        }
 
-	public void paintComponent(Graphics page) {
-		Utilities.drawBackdrop(page, backdrop, getWidth(), getHeight(), this);
-	}
+        public void paintComponent(Graphics page) {
+                Utilities.drawBackdrop(page, backdrop, getWidth(), getHeight(), this);
+        }
 
-	public void reset(boolean isBasicGame) {
-		setPiecesLeft(PitCons.initialNumberOfPieces);
-		setPiecesLost(0);
-		piecesLost.setVisible(!isBasicGame);
-		showColorText(showColorText);
-	}
+        public void reset(boolean isBasicGame) {
+                setPiecesLeft(PitCons.initialNumberOfPieces);
+                setPiecesLost(0);
+                piecesLost.setVisible(!isBasicGame);
+                showColorText(showColorText);
+        }
 
-	public String getPlayerName() {
-		if (playerName.compareTo(PitCons.nameFieldPrompt) != 0)
-			return playerName;
-		else
-			return player;
-	}
+        public String getPlayerName() {
+                //if (playerName.compareTo(PitCons.nameFieldPrompt) != 0)
+				if(nameLock)
+                        return playerName;
+                else
+                        return player;
+        }
 
-	public String getPlayer() {
-		return player;
-	}
+        public String getPlayer() {
+                return player;
+        }
+        
+        public void setNumWins(int numWins){
+            this.numWins.setText(Language.numWinsText+numWins);
+        }
 
-	public void showColorText(boolean highlightOn) {
-		Color textColor = highlightOn ? playerColor : Color.black;
-		titleLabel.setForeground(textColor);
-		nameLabel.setForeground(textColor);
-		piecesLeft.setForeground(textColor);
-		piecesLost.setForeground(textColor);
-		repaint();
-	}
+        public void showColorText(boolean highlightOn) {
+                Color textColor = highlightOn ? playerColor : Color.black;
+                titleLabel.setForeground(textColor);
+                nameLabel.setForeground(textColor);
+                piecesLeft.setForeground(textColor);
+                piecesLost.setForeground(textColor);
+                numWins.setForeground(textColor);
+                repaint();
+        }
 
-	public void setPiecesLeft(int pieceCount) {
-		this.piecesLeft.setText(pieceCount + PitCons.pieceCountText);
-	}
+        public void setPiecesLeft(int pieceCount) {
+                this.piecesLeft.setText(pieceCount + Language.pieceCountText);
+        }
 
-	public void setPiecesLost(int piecesLost) {
-		this.piecesLost.setText(piecesLost + PitCons.pieceLostText);
-	}
+        public void setPiecesLost(int piecesLost) {
+                this.piecesLost.setText(piecesLost + Language.pieceLostText);
+        }
 
-	private class PlayerListener implements MouseListener {
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			if (e.getSource() == nameLabel) {
-				String userName = JOptionPane.showInputDialog(nameLabel,
-						PitCons.nameFieldPrompt, player,
-						JOptionPane.QUESTION_MESSAGE);
+        private class PlayerListener implements MouseListener {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                        if (e.getSource() == nameLabel) {
+                                String userName = JOptionPane.showInputDialog(nameLabel,
+                                                Language.nameFieldPrompt, player,
+                                                JOptionPane.QUESTION_MESSAGE);
 
-				if (userName != null && !userName.isEmpty()) {
-					playerName = userName;
-					nameLabel.setForeground(playerColor);
-					nameLabel.setText(playerName);
-				}
-			}
-		}
+                                if (userName != null && !userName.isEmpty()) {
+                                        playerName = userName;
+                                        nameLabel.setForeground(playerColor);
+                                        nameLabel.setText(playerName);
+										nameLock = true;
+										gPanel.resetWin(pNum);
+										numWins.setText(Language.numWinsText+"0");
+                                }
+                        }
+                }
 
-		@Override
-		public void mouseEntered(MouseEvent e) {
-		}
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                }
 
-		@Override
-		public void mouseExited(MouseEvent e) {
-		}
+                @Override
+                public void mouseExited(MouseEvent e) {
+                }
 
-		@Override
-		public void mousePressed(MouseEvent e) {
-		}
+                @Override
+                public void mousePressed(MouseEvent e) {
+                }
 
-		@Override
-		public void mouseReleased(MouseEvent e) {
-		}
-	}
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                }
+        }
 }
